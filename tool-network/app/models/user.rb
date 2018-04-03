dd73c2bc1376fe3486d1579b858120f3c22c538e
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-03 12:42:04 -0400

    Add user seed data and added has_secure_password to User model

--- tool-network/app/models/user.rb
+++ tool-network/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :owned_tools, class_name: "Tool"
   has_many :reviews
   has_many :borrowed_tools, -> {distinct}, through: :reviews, source: :tool
+
+  has_secure_password
 end
--- tool-network/db/seeds.rb
+++ tool-network/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+
+dmitry = User.create(name: "Dmitry", email: "dmitry@gmail.com", password: "test", password_confirmation: "test", location: "Dundas", photo: "no photo")
+
+alex = User.create(name: "Alex", email: "alex@gmail.com", password: "test", password_confirmation: "test", location: "King", photo: "no photo")
+
+debbie = User.create(name: "Debbie", email: "debbie@gmail.com", password: "test", password_confirmation: "test", location: "Junction", photo: "no photo")
+
+bill = User.create(name: "Bill", email: "bill@gmail.com", password: "test", password_confirmation: "test", location: "Markham", photo: "no photo")
