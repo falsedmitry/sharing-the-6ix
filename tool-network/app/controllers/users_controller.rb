18da7981da88e469176a22995e5e5c04fb99977d
aubanuet <aubanuet@gmail.com>
2018-04-03 12:32:08 -0400

    Add methods for the User controller

--- tool-network/app/controllers/users_controller.rb
+++ tool-network/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+
+  end
+
+  def destroy
+  end
+
 end
