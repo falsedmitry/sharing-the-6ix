0ba317f70a9276bb018ba909d21ec6303d5acc58
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-03 16:35:55 -0400

    Add FactoryBot gem, created factories for all models, and tested rating method for User model

--- tool-network/test/models/user_test.rb
+++ tool-network/test/models/user_test.rb
@@ -4,4 +4,57 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "rating is 0.0 if under 5 reviews" do
+    user = FactoryBot.create(:user)
+    4.times do
+      tool = FactoryBot.create(:tool, user_id: user.id)
+      FactoryBot.create(:review, tool_id: tool.id, user_id: user.id)
+    end
+
+    expected = 0.0
+    actual = user.rating
+    assert_equal(expected, actual)
+  end
+
+  test "rating is 1.0 if 5 reviews, all 1 stars" do
+    user = FactoryBot.create(:user)
+    5.times do
+      tool = FactoryBot.create(:tool, user_id: user.id)
+      FactoryBot.create(:review, rating: 1, tool_id: tool.id, user_id: user.id)
+    end
+
+    expected = 1.0
+    actual = user.rating
+    assert_equal(expected, actual)
+  end
+
+  test "rating is 3.0 if 10 reviews, all 3 stars" do
+    user = FactoryBot.create(:user)
+    10.times do
+      tool = FactoryBot.create(:tool, user_id: user.id)
+      FactoryBot.create(:review, rating: 3, tool_id: tool.id, user_id: user.id)
+    end
+
+    expected = 3.0
+    actual = user.rating
+    assert_equal(expected, actual)
+  end
+
+  test "rating is 4.0 if 10 reviews, 5 5 stars, 5 3 stars" do
+    user = FactoryBot.create(:user)
+    5.times do
+      tool = FactoryBot.create(:tool, user_id: user.id)
+      FactoryBot.create(:review, rating: 5, tool_id: tool.id, user_id: user.id)
+    end
+
+    5.times do
+      tool = FactoryBot.create(:tool, user_id: user.id)
+      FactoryBot.create(:review, rating: 3, tool_id: tool.id, user_id: user.id)
+    end
+
+    expected = 4.0
+    actual = user.rating
+    assert_equal(expected, actual)
+  end
 end
