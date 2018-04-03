f954350763e6942f8c4fa955775abf837697714b
debbie <drosenfeld87@gmail.com>
2018-04-03 12:25:02 -0400

    routes created

--- tool-network/config/routes.rb
+++ tool-network/config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'tools#index'
+
+  resources :tools do
+    resources :reviews do
+      resources :images
+    end
+  end
+  
+  resources :users
 end
