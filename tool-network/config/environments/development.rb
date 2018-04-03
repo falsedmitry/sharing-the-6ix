27d317c6595e8c0f3fb2598e89ce67769acce08e
debbie <drosenfeld87@gmail.com>
2018-04-03 14:11:17 -0400

    add index and show page

--- tool-network/config/environments/development.rb
+++ tool-network/config/environments/development.rb
@@ -26,10 +26,23 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+   address:              ‘smtp.gmail.com’,
+   port:                 587,
+   user_name:            ‘toolnetwork2018@gmail.com’,
+   password:             ENV[“GMAIL_KEY”],
+   authentication:       ‘plain’,
+   enable_starttls_auto: true  }
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.default_options = {from: ‘toolnetwork2018@gmail.com’}
 
-  config.action_mailer.perform_caching = false
+   host = ‘localhost:3000’                     # Local server
+   config.action_mailer.default_url_options = { host: host, protocol: ‘http’ }
+
+  # Don't care if the mailer can't send.
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- tool-network/db/seeds.rb
+++ tool-network/db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+Image.destroy_all
+Review.destroy_all
+Tool.destroy_all
+
+User.destroy_all
+
+dmitry = User.create(name: "Dmitry", email: "dmitry@gmail.com", password: "test", password_confirmation: "test", location: "Dundas", photo: "no photo")
+
+alex = User.create(name: "Alex", email: "alex@gmail.com", password: "test", password_confirmation: "test", location: "King", photo: "no photo")
+
+debbie = User.create(name: "Debbie", email: "debbie@gmail.com", password: "test", password_confirmation: "test", location: "Junction", photo: "no photo")
+
+bill = User.create(name: "Bill", email: "bill@gmail.com", password: "test", password_confirmation: "test", location: "Markham", photo: "no photo")
+
+# Image.create(:category_name => 'technology')
+# Image.create(:category_name =>'art')
+# Image.create(:category_name => 'sports')
+# Image.create(:category_name => 'food')
+
+
+10.times do
+  tool = Tool.create!(
+              name: Faker::App.name,
+              description: Faker::Lorem.paragraph,
+              condition: rand(10),
+              owner: User.first,
+              on_loan: false,
+
+            )
+            # tool.categories = [Image.first]
+
+  5.times do
+    tool.reviews.create!(
+      comment: Faker::Lorem.paragraph,
+      rating: rand(5),
+      user: User.first,
+      tool: Tool.first
+    )
+  end
+end
+
+
+
+# 20.times do
+#   tool = Tool.all.sample
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Pledge.create!(
+#     user: User.all[1..-1].sample,
+#     tool: tool,
+#     dollar_amount: tool.rewards.sample.dollar_amount + rand(10)+1
+#   )
+# end
