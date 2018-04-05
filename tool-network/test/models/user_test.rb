18a8836c1f4d41791a5e7e71a52995a2c9cee5db
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-05 16:43:16 -0400

    Add more tests to user model

--- tool-network/test/models/user_test.rb
+++ tool-network/test/models/user_test.rb
@@ -7,6 +7,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "rating is 0.0 if under 5 reviews" do
     user = FactoryBot.create(:user)
+
     4.times do
       tool = FactoryBot.create(:tool, user_id: user.id)
       FactoryBot.create(:review, tool_id: tool.id, user_id: user.id)
@@ -19,6 +20,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "rating is 1.0 if 5 reviews, all 1 stars" do
     user = FactoryBot.create(:user)
+
     5.times do
       tool = FactoryBot.create(:tool, user_id: user.id)
       FactoryBot.create(:review, rating: 1, tool_id: tool.id, user_id: user.id)
@@ -31,6 +33,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "rating is 3.0 if 10 reviews, all 3 stars" do
     user = FactoryBot.create(:user)
+
     10.times do
       tool = FactoryBot.create(:tool, user_id: user.id)
       FactoryBot.create(:review, rating: 3, tool_id: tool.id, user_id: user.id)
@@ -43,6 +46,7 @@ class UserTest < ActiveSupport::TestCase
 
   test "rating is 4.0 if 10 reviews, 5 5 stars, 5 3 stars" do
     user = FactoryBot.create(:user)
+
     5.times do
       tool = FactoryBot.create(:tool, user_id: user.id)
       FactoryBot.create(:review, rating: 5, tool_id: tool.id, user_id: user.id)
@@ -55,6 +59,34 @@ class UserTest < ActiveSupport::TestCase
 
     expected = 4.0
     actual = user.rating
+    assert_equal(expected, actual)
+  end
+
+  test "tools_out_on_loan_is_empty_if_no_tools_out_on_loan" do
+    user = FactoryBot.create(:user)
+
+    5.times do
+      tool = FactoryBot.create(:tool, user_id: user.id)
+    end
+
+    expected = []
+    actual = user.tools_out_on_loan
+    assert_equal(expected, actual)
+  end
+
+  test "tools_out_on_loan_contains_tools_if_they_are_on_loan" do
+    user = FactoryBot.create(:user)
+
+    3.times do
+      tool = FactoryBot.create(:tool, user_id: user.id, on_loan: true)
+    end
+
+    2.times do
+      tool = FactoryBot.create(:tool, user_id: user.id)
+    end
+
+    expected = 3
+    actual = user.tools_out_on_loan.size
     assert_equal(expected, actual)
   end
 end
