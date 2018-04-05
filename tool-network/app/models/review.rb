d7dae8edef52b4789cbfc825b2fdc0d721464a64
Dmitry Serbin <33602208+dmitrysbn@users.noreply.github.com>
2018-04-05 13:30:06 -0400

    Update review.rb

--- tool-network/app/models/review.rb
+++ tool-network/app/models/review.rb
@@ -5,14 +5,5 @@ class Review < ApplicationRecord
 
   validates :rating, :comment, presence: true
   validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
-
-
-  def has_pictures
-    unless self.images.any?
-      self.errors[:review] << "must have images"
-    end
-  end
-
-
-
+  
 end
