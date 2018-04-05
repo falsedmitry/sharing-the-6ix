d05a7414c71f40c310849b276ae0a696398e0d07
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-05 15:50:02 -0400

    Add Loan and Image validations, error partial

--- tool-network/app/models/image.rb
+++ tool-network/app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
   belongs_to :tool
   belongs_to :review
+
+  validates :description, :url, presence: true
 end
--- tool-network/app/models/loan.rb
+++ tool-network/app/models/loan.rb
@@ -2,4 +2,21 @@ class Loan < ApplicationRecord
   belongs_to :borrower, class_name: "User", foreign_key: 'user_id'
   belongs_to :tool
 
+  validates :start_date, :due_date, presence: true
+
+  validate :start_date_cannot_be_in_the_past
+  validate :due_date_is_after_start_date
+
+  def start_date_cannot_be_in_the_past
+    if start_date && start_date < Date.today
+      errors[:loan_date] << 'cannot be in the past'
+    end
+  end
+
+  def due_date_is_after_start_date
+    if due_date && start_date && due_date < start_date
+      errors[:due_date] << 'cannot be before loan date'
+    end
+  end
+
 end
