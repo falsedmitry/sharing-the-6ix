5b41ee6b6356463f468d458bd6d83dc8030101aa
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-07 15:00:26 -0400

    Add map to neighbourhood show page and fill out the boundaries

---
+++ tool-network/app/assets/javascripts/main.js
@@ -0,0 +1,19 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//
+//   function initMap() {
+//     var toronto = {lat: -25.363, lng: 131.044};
+//
+//     var map = new google.maps.Map(
+//       document.querySelector('#map'), {
+//       zoom: 4,
+//       center: toronto
+//     });
+// 
+//     var marker = new google.maps.Marker({
+//       position: toronto,
+//       map: map
+//     });
+//   }
+//
+//   console.log("initMap should now exist");
+// })
--- tool-network/app/controllers/neighbourhoods_controller.rb
+++ tool-network/app/controllers/neighbourhoods_controller.rb
@@ -8,5 +8,25 @@ class NeighbourhoodsController < ApplicationController
     @users = @neighbourhood.users
     @tools = @neighbourhood.tools
 
+    address = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=Little+Portugal&bounds=43.855458,-79.002481|43.458297,-79.639219&key=AIzaSyA4smff7b389AgWQAZkI1CqrR2nB7cs0xM")
+
+    address_body = JSON.parse(address.body)
+    puts address_body["resuts"]
+
+    location = address_body["results"][0]["geometry"]["location"]
+    bounds = address_body["results"][0]["geometry"]["bounds"]
+
+    northeast = bounds["northeast"]
+    southwest = bounds["southwest"]
+
+    @north = northeast["lat"]
+    @east = northeast["lng"]
+    @south = southwest["lat"]
+    @west = southwest["lng"]
+
+    @lat = location["lat"]
+    @lng = location["lng"]
+
+
   end
 end
