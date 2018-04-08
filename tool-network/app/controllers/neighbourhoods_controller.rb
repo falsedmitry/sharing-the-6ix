516758246d06e707da5bc9c769029f7e2298fe4c
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-08 13:32:28 -0400

    Add dynamic google maps api call to neighbourhoods controller

--- tool-network/app/controllers/neighbourhoods_controller.rb
+++ tool-network/app/controllers/neighbourhoods_controller.rb
@@ -6,9 +6,40 @@ class NeighbourhoodsController < ApplicationController
   def show
     @neighbourhood = Neighbourhood.find(params[:id])
     @users = @neighbourhood.users
+
+    @locations = []
+    @users.each do |user|
+      @locations << user.location
+    end
+
     @tools = @neighbourhood.tools
 
-    address = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=Little+Portugal&bounds=43.855458,-79.002481|43.458297,-79.639219&key=AIzaSyA4smff7b389AgWQAZkI1CqrR2nB7cs0xM")
+    case @neighbourhood.name
+    when "Junction"
+      code = "The+Junction,Toronto"
+    when "Beaches"
+      code = "The+Beach,Toronto"
+    when "Dundas West"
+      code = "Little+Portugal,Toronto"
+    when "Little Italy"
+      code = "Little+Italy"
+    when "Kensington Market"
+      code = "Kensington+Market"
+    when "Yonge & Eglinton"
+      code = "Midtown+Toronto"
+    when "East York"
+      code = "East+York"
+    when "Danforth"
+      code = "Greektown"
+    when "Liberty Village"
+      code = "Liberty+Village"
+    when "North York"
+      code = "North+York"
+    else
+      code = @neighbourhood.name
+    end
+
+    address = HTTParty.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{code},Toronto&bounds=43.855458,-79.002481|43.458297,-79.639219&key=AIzaSyA4smff7b389AgWQAZkI1CqrR2nB7cs0xM")
 
     address_body = JSON.parse(address.body)
     puts address_body["resuts"]
