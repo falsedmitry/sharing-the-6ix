cdca33ab221f04729957878bde5fbf1bd37eae49
debbie <drosenfeld87@gmail.com>
2018-04-10 11:37:07 -0400

    dropdown works in responsive view

--- tool-network/app/assets/javascripts/navbar.js
+++ tool-network/app/assets/javascripts/navbar.js
@@ -1,5 +1,13 @@
 document.addEventListener("turbolinks:load", function(){
   var menu = document.querySelector("#hamburger-menu")
+  var dropdownContent = document.querySelector('.dropdown-content')
   menu.addEventListener('click', function() {
+    dropdownContent.style.display = 'block'
+    if (dropdownContent.style.visibility == 'hidden') {
+      dropdownContent.style.visibility = 'visible'
+    }
+    else {
+      dropdownContent.style.visibility = 'hidden'
+    }
   })
 })
