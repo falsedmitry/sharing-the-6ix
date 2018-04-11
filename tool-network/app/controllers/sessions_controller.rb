864813157475ebee4bbab80c7f9e039a559ab009
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-11 11:01:38 -0400

    Add neighbourhood images as seed

--- tool-network/app/controllers/sessions_controller.rb
+++ tool-network/app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "You have successfully login as #{user.email}!"
+      flash[:notice] = "You have successfully logged in as #{user.email}!"
       redirect_to root_url
     else
       flash[:alert] = "Email or Password invalid!"
