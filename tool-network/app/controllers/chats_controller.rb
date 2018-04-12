6bb6c7608e943a4f013ff001ee6f997a791d005c
likwokbiu <li_bill@ymail.com>
2018-04-12 16:55:42 -0400

    chat enhancement to allow 2 way chat in chatroom

--- tool-network/app/controllers/chats_controller.rb
+++ tool-network/app/controllers/chats_controller.rb
@@ -1,6 +1,9 @@
 require "json"
 
 class ChatsController < ApplicationController
+  before_action :load_chat, only: [:edit, :update]
+  before_action :require_login
+  before_action :require_user_authority, only: [:edit, :update]
 
   def index
     @chats = Chat.all.select("max(id) as id", :user_id, :tool_id).group(:tool_id, :user_id).order(:tool_id, :user_id)
@@ -38,23 +41,34 @@ class ChatsController < ApplicationController
   end
 
   def edit
-    chat = Chat.find(params[:id])
-    Chat.where("tool_id = ?", chat.tool_id).where("user_id = ?", chat.user.id).where("owner_reply = ?", false).update_all(unread: false)
-    @chats = Chat.where("tool_id = ?", chat.tool_id).where("user_id = ?", chat.user.id).order(created_at: :asc)
+    if current_user == @chat0.tool.owner
+      Chat.where("tool_id = ?", @chat0.tool_id).where("user_id = ?", @chat0.user.id).where("owner_reply = ?", false).update_all(unread: false)
+    else
+      Chat.where("tool_id = ?", @chat0.tool_id).where("user_id = ?", @chat0.user.id).where("owner_reply = ?", true).update_all(unread: false)
+    end
+    @chats = Chat.where("tool_id = ?", @chat0.tool_id).where("user_id = ?", @chat0.user.id).order(created_at: :asc)
     @chat = @chats[-1]
   end
 
   def update
-    chat = Chat.find(params[:id])
     @chat = Chat.new
     @chat.content = params[:chat][:content]
-    @chat.tool = chat.tool
-    @chat.user = chat.user
+    @chat.tool = @chat0.tool
+    @chat.user = @chat0.user
     @chat.unread = true
-    @chat.owner_reply = true
+    if current_user == @chat0.tool.owner
+      @chat.owner_reply = true
+    else
+      @chat.owner_reply = false
+    end
     if @chat.save
-      UserMailer.requester_chat(@chat).deliver_now
-      flash[:notice] = "The message is sent to the requester with email notification."
+      if current_user == @chat0.tool.owner
+        UserMailer.requester_chat(@chat).deliver_now
+        flash[:notice] = "The message is sent to the requester with email notification."
+      else
+        UserMailer.owner_chat(@chat).deliver_now
+        flash[:notice] = "The message is sent to the owner with email notification."
+      end
     else
       flash[:alert] = "The message is failed in sending to the requester."
     end
@@ -75,4 +89,20 @@ class ChatsController < ApplicationController
       redirect_to edit_chat_url(params[:id])
     end
   end
+
+  def load_chat
+    @chat0 = Chat.find(params[:id])
+  end
+
+  def require_user_authority
+    unless current_user == @chat0.user || current_user == @chat0.tool.owner
+      flash[:alert] = "You are not authorized to modify this chat."
+      redirect_to login_url
+    end
+  end
+
+  def ajax_update
+
+  end
+
 end
--- tool-network/config/environments/production.rb
+++ tool-network/config/environments/production.rb
@@ -62,6 +62,22 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "tool-network_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  #email setup
+  config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                 587,
+   user_name:            'toolnetwork2018@gmail.com',
+   password:             ENV["GMAIL_KEY"],
+   authentication:       'plain',
+   enable_starttls_auto: true  }
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.default_options = {from: 'toolnetwork2018@gmail.com'}
+
+   host = 'limitless-dawn-77973.herokuapp.com'    # Production server
+   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
