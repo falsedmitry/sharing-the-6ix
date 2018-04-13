4b7334c05ca2dbff78c1f4aa8e4cd47b83859c67
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-13 12:47:10 -0400

    minor typo fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,24 +36,5 @@ class User < ApplicationRecord
     end
 
     currently_borrowed_tools
-
   end
-
-  # def rating
-  #   rating = 0.0
-  #   number_of_reviews = 0
-  #
-  #   owned_tools.each do |tool|
-  #     tool.reviews.each do |review|
-  #       rating += review.rating
-  #       number_of_reviews += 1
-  #     end
-  #   end
-  #
-  #   if number_of_reviews < 5
-  #     0
-  #   else
-  #     rating / number_of_reviews
-  #   end
-  # end
 end
