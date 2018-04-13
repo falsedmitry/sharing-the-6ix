a329b2000b63191abdb9f9e8a0bdb72b5939c674
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-13 13:41:55 -0400

    Fix dropdown menu

--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class ChatsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get chats_url
-    assert_response :success
+    assert_response :found
   end
 end
