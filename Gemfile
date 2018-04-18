d2f645c3891a523d074230911a9de57a1aaafe74
debbie <drosenfeld87@gmail.com>
2018-04-18 18:17:52 -0400

    scroll to top function is working

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
--- app/assets/javascripts/back-to-top-btn.js
+++ app/assets/javascripts/back-to-top-btn.js
@@ -1,19 +1,16 @@
-document.addEventListener('turbolinks:load', function(){
-// When the user scrolls down 20px from the top of the document, show the button
-  var backToTopButton = document.querySelector('#back-to-top-btn');
-  window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
   function scrollFunction() {
       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          document.getElementById('back-to-top-btn').style.display = 'block';
+          document.getElementById("back-to-top-btn").style.display = "block";
       } else {
-          document.getElementById("back-to-top-btn").style.visibility = 'visible';
+          document.getElementById("back-to-top-btn").style.display = "none";
       }
   }
 
   // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
-}
+//}
