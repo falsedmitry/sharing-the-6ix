5989414cb83b85e93ea883102ce66e2f6c101250
dmitrysbn <dmitry.sbn@gmail.com>
2018-04-20 13:00:19 -0400

    Add if condition to top button js file

--- app/assets/javascripts/back-to-top-btn.js
+++ app/assets/javascripts/back-to-top-btn.js
@@ -1,16 +1,19 @@
-window.onscroll = function() {scrollFunction()};
+var backToTopButton = document.querySelector('#back-to-top-btn');
 
+if (backToTopButton) {
   function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-          document.getElementById("back-to-top-btn").style.display = "block";
-      } else {
-          document.getElementById("back-to-top-btn").style.display = "none";
-      }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("back-to-top-btn").style.display = "block";
+    } else {
+      document.getElementById("back-to-top-btn").style.display = "none";
+    }
   }
 
   // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
-      document.body.scrollTop = 0; // For Safari
-      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
-//}
+
+  window.onscroll = function() {scrollFunction()};
+}
--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,15 +1,16 @@
 document.addEventListener("turbolinks:load", function(){
-  var menu = document.querySelector("#hamburger-menu")
-  var dropdownContent = document.querySelector('.dropdown-content')
+  var menu = document.querySelector("#hamburger-menu");
+  var dropdownContent = document.querySelector('.dropdown-content');
+
   menu.addEventListener('click', function() {
-    dropdownContent.style.display = 'block'
+    dropdownContent.style.display = 'block';
     console.log(dropdownContent.style.visibility);
+
     if (dropdownContent.style.visibility !== 'visible') {
-      dropdownContent.style.visibility = 'visible'
+      dropdownContent.style.visibility = 'visible';
     }
     else {
-      dropdownContent.style.visibility = 'hidden'
-
+      dropdownContent.style.visibility = 'hidden';
     }
   })
 })
